6eebbeb6efd2446dd7e3ab8ae6752feac008bc4b
DataBook4U (Sebastian) <opocensky.sebastian@outlook.de>
2025-06-01 12:15:00 +0200

    start plot prep.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 iris = pd.read_csv("iris.csv")
 
 print(iris.head())
 
+sns.scatterplot (x='sepal length')
 iris_setosa = iris["variety"]
 
 print(iris_setosa.head())
