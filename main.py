792a203e6e065300d6776d2ca2a9f4f13297bcef
DataBook4U (Sebastian) <opocensky.sebastian@outlook.de>
2025-06-01 18:33:52 +0200

    First basic plot (scatter)

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 iris = pd.read_csv("iris.csv")
+df = iris
+
 
 print(iris.head())
 
-sns.scatterplot (x='sepal length')
-iris_setosa = iris["variety"]
+sns.scatterplot(x='sepal.length', y='sepal.width', style='variety', data=df)
 
-print(iris_setosa.head())
 
-print('Test3')
+plt.show()
