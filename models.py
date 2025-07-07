6a36c383c299724b8d582cc0c071a4d4a86dcbc0
Sebastian Opocensky <opocensky.sebastian@outlook.de>
2025-07-07 16:35:05 +0200

    All back to the beginning

--- models.py
+++ models.py
@@ -2,20 +2,6 @@
 The models.py loads the ml models
 """
 
-
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 
-class KNNModel:
-    def __init__(self, n_neighbors=3):
-        self.model = KNeighborsClassifier(n_neighbors=n_neighbors)
-
-    def train(self, X_train, y_train):
-        self.model.fit(X_train, y_train)
-
-    def evaluate(self, X_test, y_test):
-        predictions = self.model.predict(X_test)
-        return accuracy_score(y_test, predictions)
-
-    def predict(self, X_new):
-        return self.model.predict(X_new)
