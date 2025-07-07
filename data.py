896fee4f3e3a791e090e43d140c964222e16e15f
Sebastian Opocensky <opocensky.sebastian@outlook.de>
2025-07-07 16:44:38 +0200

    table (head) output of data

--- data.py
+++ data.py
@@ -1,10 +1,17 @@
 """
 The data.py loads and performs changes on the dataset
 """
-
+import pandas as pd
 from sklearn.datasets import load_iris                      #Stellt Iris Datensatz bereit
 from sklearn.model_selection import train_test_split        #Aufteilung in Test u. Trainingsdaten
 from sklearn.preprocessing import StandardScaler            #Ermöglicht Standardisierung der Features
 
 
+#Maybe move to seperate file like "explore.py"??
 
+iris = load_iris()
+
+df = pd.DataFrame(data=iris, columns=iris.feature_names)
+df['target'] = iris.target
+
+print(df.head())
