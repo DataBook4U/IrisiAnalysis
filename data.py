44595e83d21cf31922b532afd193bcdd7d661d21
Sebastian Opocensky <opocensky.sebastian@outlook.de>
2025-07-10 18:29:00 +0200

    added function to get the Null Values of all columns

--- data.py
+++ data.py
@@ -30,20 +30,29 @@ class TransformData:
 
 class ExploreData:
 
-    def __init__(self, DataFrame, DataColumn):
+    def __init__(self, DataFrame, DataFrameColumns):
         self.DataFrame = DataFrame
-        self.DataColumn = DataColumn
+        self.DataFrameColumns = DataFrameColumns
         self.average = None     #to get average value of a column
-        self.HasNull = None     #to get amount of Null values in a column
+        self.HasNull = 0    #to get amount of Null values in a column
 
-    def GetAverage(self):
-        self.average = self.DataFrame[self.DataColumn].mean()
-        print("Average value of " + self.DataColumn + " : " + str(self.average))
-
-    def GetAverageAll(self, columns):
-        for column in columns:
+    #Berechne den Mittelwert für eine Spalte im Datensatz:
+    def GetAverageAll(self):
+        for column in self.DataFrameColumns[:-1]:                                     #the last element is the target column that I don't want to look at for now
             self.average = self.DataFrame[column].mean()
             print("Average value of " + column + " : " + str(self.average))
+
+    #Gib mir an wie viele Null Werte jeweils in meinen Spalten enthalten sind:
+    def GetNullAll(self):
+        for column in self.DataFrameColumns:
+            for value in column:
+                if value == "NaN":
+                    self.HasNull += 1
+                else:
+                    self.HasNull = self.HasNull
+            print("The column " + column + " has: " + str(self.HasNull) + " values.")
+
+
 
 #Load Iris DataSet from Library
 iris = load_iris()
@@ -56,7 +65,7 @@ print(columns)
 #print(transformer.df)
 
 #Get Data Insights
-test1 = ExploreData(df_iris, "petal length (cm)")
-test1.GetAverageAll(columns)
+test1 = ExploreData(df_iris, columns)
+test1.GetNullAll()
 
 #Create a function that goes through the DF by all the columns and prints out their average values
